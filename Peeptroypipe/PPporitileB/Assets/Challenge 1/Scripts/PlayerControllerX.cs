c2eb94e870a2151970cb112971017c17a57b8f64
Unknown <stafford.carh@gmail.com>
2020-11-25 18:59:25 -0500

    Challenge1 Complete
    completed challenge 1, with a pretty decent plane ludoergonomics. Mainly I had to look into my Preferences and see that the wrong axis was tagged

--- Peeptroypipe/PPporitileB/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Peeptroypipe/PPporitileB/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,35 +4,74 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turnSpeed = 30.0f;
-    private float horizontalInput;
-    private float verticalInput;
-    private Vector3 planeTransformCoords;
+    [SerializeField] private float speed = 20.0f;
+    [SerializeField] private float turnSpeed = 30.0f;
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float verticalInput;
+    public Vector3 planeTransformCoords;
+    public Vector3 planeTransformRots;
+    public Vector3 lerpAttempt;
+    
 
     // Start is called before the first frame update
     void Start()
-    {
+    {        
+    }
 
-        
+    void PlaneTurnFunc() 
+    {
+        //// get the user's input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // tilt the plane based on arrow keys
+
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
+        transform.Rotate(Vector3.right * -verticalInput * Time.deltaTime * turnSpeed);
+
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+    }
+
+    void PlaneRightingItself() 
+    {
+        //Debug.Log("using lerpAttempt " + planeTransformRots + " is the rotation value");
+        //an attempt to make the plane level when no keys are depressed
+        gameObject.transform.Rotate(lerpAttempt);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // get the user's input
-        planeTransformCoords = new Vector3(0, horizontalInput + verticalInput, 0);
 
+        if (planeTransformRots.x < 180 || planeTransformRots.x > 0)
+        {
+            planeTransformRots.x *= -1;
+        }
+        if (planeTransformRots.y < 180 || planeTransformRots.y > 0)
+        {
+            planeTransformRots.y *= -1;
+        }
+        if (planeTransformRots.z < 180 || planeTransformRots.z > 0)
+        {
+            planeTransformRots.z *= -1;
+        }
 
-        verticalInput = Input.GetAxis("Vertical");
-        //horizontalInput = Input.GetAxis("Horizontal");
-
+        PlaneTurnFunc();        
 
         // plane going forward at consistant speed
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // plane's current Rotation
+        planeTransformRots = new Vector3(gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z);
+        
+        lerpAttempt = Vector3.Lerp(planeTransformRots, -planeTransformRots, 1000.0f * Time.deltaTime);
+        
+        if (verticalInput == 0 && horizontalInput == 0) 
+        {
+            PlaneRightingItself();
+        }
 
-        // tilt the plane reight/left based on the right/left arrow keys
-        transform.Translate( Vector3.forward * verticalInput * Time.deltaTime * speed);
-        transform.Rotate(Vector3.forward * verticalInput  * Time.deltaTime* turnSpeed);
     }
 }
