1551d83b76f916f63b8b5934f72d37afe572a269
Unknown <stafford.carh@gmail.com>
2020-11-30 17:12:14 -0500

    Added Files from 3D Assets
    Moved some of the course files into game

---
+++ Peeptroypipe/PersProj-1/Barrier Break/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -5);
+
+    private void Awake()
+    {
+        if (player == null) 
+        {
+            Debug.Break();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Offest the camera behind the player by adding to the player's position
+        transform.position = player.transform.position + offset;
+    }
+}
