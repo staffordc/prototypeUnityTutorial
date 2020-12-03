15e63542c457ae30a0c87507966f02a6331aba8c
Unknown <stafford.carh@gmail.com>
2020-12-03 14:28:26 -0500

    PreBallTutorial
    This is about as far as I got before needing to look into other solutions, and do a tutorial in another version of Unity

---
+++ Peeptroypipe/PersProj-1/Barrier Break/Assets/Scripts/AngleCheckTool.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleCheckTool : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.GetComponent<MeshRenderer>().isVisible == true)
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = false;            
+        }
+    }
+
+    void CheckAngle()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckAngle();
+        
+    }
+}
--- Peeptroypipe/PersProj-1/Barrier Break/Assets/Scripts/PlayerController.cs
+++ Peeptroypipe/PersProj-1/Barrier Break/Assets/Scripts/PlayerController.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Private Variables
-    private float speed = 20.0f;
-    private float turnSpeed = 90.0f;
+    private float speed = 8.0f;
+    private float turnSpeed = 10.0f;
     private float horizontalInput;
     private float forwardInput;
+
+    //private float clampRotation;
 
     // Update is called once per frame
     void Update()
     {
         // This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        forwardInput = Input.GetAxis("Vertical");        
 
-        // Moves the car forward based on vertical input
+        //What angle do we have as speedup 
+        //slowdown angle 
+        //how do we measure the angle
+
+        // Moves the player forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        // Rotates the car based on horizontal input
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        // Rotates the player based on horizontal input
+        transform.Rotate(Vector3.up *  Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
