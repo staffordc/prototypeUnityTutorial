6467e809c01962537fc983101ca8db547e7acf4e
Unknown <stafford.carh@gmail.com>
2021-02-16 17:12:28 -0500

    Initializing Unit 2y
    new Unity scene, and some of the changes for the start of Unit 2's prototype

---
+++ Peeptroypipee2/Lesson 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float offScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float positionZed = gameObject.transform.position.z;
+
+        if (positionZed >= offScreen || positionZed <= -offScreen)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Peeptroypipee2/Lesson 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    public int animalIndex;
+    public float xRangeLimit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //xRangeLimit = Math.Random();
+        
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            animalIndex = Random.Range(0, animalPrefabs.Length);
+            xRangeLimit = Random.Range(-10, 11);
+            Instantiate(animalPrefabs[animalIndex], new Vector3(xRangeLimit, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
